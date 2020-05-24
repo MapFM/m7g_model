17d3112236190861b9f79700d3a6d52096383b12
MapFM <1326278618@qq.com>
2020-05-24 22:11:08 +0800

    delete

--- py/npftocsv.py
+++
@@ -1,9 +0,0 @@
-f=open("pse2.txt","r").readlines()
-out=open("npf.csv","w")
-for line in f:
-    a=line.strip("\n").rstrip("\t").split("\t")
-    for i in a:
-        i = i.split(':')
-        for chr in i:
-            out.write(chr+',')
-    out.write('\n')
